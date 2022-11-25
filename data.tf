437c7e1b6b74fc61fe5a9cc42ddb440a73685db4
BryantLogan <bryant.logan1@gmail.com>
2022-11-25 14:03:52 -0800

    changes source_file in data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,12 @@
 # --- Packages python files into .zip to be used by Lambda functions --- #
 data "archive_file" "add_count_zip" {
   type        = "zip"
-  source_file  = "\\python\\add_count_function\\add_count.py"
+  source_file  = "\\backend\\python\\add_count_function\\add_count.py"
   output_path = "add_count.zip"
 }
 
 data "archive_file" "get_count_zip" {
   type        = "zip"
-  source_file  = "\\python\\get_count_function\\get_count.py"
+  source_file  = "\\backend\\python\\get_count_function\\get_count.py"
   output_path = "get_count.zip"
 }
