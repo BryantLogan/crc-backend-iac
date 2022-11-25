ea620818783c2f1df2e8d7f3c67eb009156c1c39
BryantLogan <bryant.logan1@gmail.com>
2022-11-25 14:10:26 -0800

    change source dir in data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,12 @@
 # --- Packages python files into .zip to be used by Lambda functions --- #
 data "archive_file" "add_count_zip" {
   type        = "zip"
-  source_dir  = "~\\python\\add_count_function\\add_count.py"
+  source_dir  = "~\\python\\add_count_function\\"
   output_path = "add_count.zip"
 }
 
 data "archive_file" "get_count_zip" {
   type        = "zip"
-  source_dir  = "~\\python\\get_count_function\\get_count.py"
+  source_dir  = "~\\python\\get_count_function\\"
   output_path = "get_count.zip"
 }
