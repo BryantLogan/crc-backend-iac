64c1ac13ee84e72ed2af02e835b5f3eb3c6fdf1d
BryantLogan <bryant.logan1@gmail.com>
2022-11-27 10:43:32 -0800

    remove content-type from headers in lambda

--- add_count.py
+++ add_count.py
@@ -18,7 +18,7 @@ def add_count_handler(event, context):
     return {
     'statusCode': 200,
     'headers': {
-        "Content-Type": "application/json",
+        # "Content-Type": "application/json",
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': '*',
