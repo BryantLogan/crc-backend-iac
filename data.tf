f7ee0b60582a2c190b483870a5b3c30c973efac6
BryantLogan <bryant.logan1@gmail.com>
2022-12-12 07:31:08 -0800

    removed unneeded python function and code

--- data.tf
+++ data.tf
@@ -4,9 +4,3 @@ data "archive_file" "add_count_zip" {
   source_file = "add_count.py"
   output_path = "add_count.zip"
 }
-
-data "archive_file" "get_count_zip" {
-  type        = "zip"
-  source_file = "get_count.py"
-  output_path = "get_count.zip"
-}
